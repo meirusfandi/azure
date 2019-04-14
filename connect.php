c67fe64895112edfa778fb96ab78a2d4d068a496
meirusfandi <meirusfandi100@gmail.com>
2019-04-15 01:28:54 +0700

    update connect

--- connect.php
+++ connect.php
@@ -6,7 +6,14 @@
     $dbname = "transferin";
 
     try {
-        $connect = new PDO("sqlsrv:server = $hostname; Database = $dbname", $username, $pass);
+        //$connect = new PDO("sqlsrv:server = $hostname; Database = $dbname", $username, $pass);
+        
+        $options = array(
+            "Database" => $dbname,
+            "UID" => $username,
+            "PWD" =>$pass
+        );
+        $connect = sqlsrv_connect($hostname, $options);
         $connect->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed Connect Database : " . $e;
