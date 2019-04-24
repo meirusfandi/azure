d5a846ae2da26015279380ac50c278ca67666f73
meirusfandi <meirusfandi100@gmail.com>
2019-04-25 05:47:33 +0700

    update source

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Azure Academy | Menjadi Azure Cloud Developer</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
 </head>
 
 <body>
